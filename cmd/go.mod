2111d94d43cf91773580fdb416988f949c2f11b9
Abhi <XCalibuR5678@proton.me>
2026-01-17 19:23:39 +0530

    Refactor command structure and add error handling in main function

--- cmd/go.mod
+++
@@ -1,3 +0,0 @@
-module XCalibur5678/Aurora/cmd
-
-go 1.25.6
--- main.go
+++ main.go
@@ -1,6 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/XCalibur5678/Aurora/cmd"
+	"github.com/urfave/cli/v3"
+	"log"
+	"os"
 )
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "Aurora",
+		Usage: "A simple yet effective AUR helper",
+		Action: func(context.Context, *cli.Command) error {
+			fmt.Println("Hello friend!")
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
