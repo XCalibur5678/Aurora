64b8b2ad8456384cea2d6b79e7a7d7f414f8ac1d
Abhi <abhigyan@abhigyanchatterjee.in>
2026-05-03 03:14:22 +0530

    Add runtime validation for Arch based distro or not

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -9,6 +11,13 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "aurora",
 	Short: "Aurora is a CLI based AUR helper , which enables users to easily search, install and manage packages from the Arch User Repository (AUR).",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if _, err := exec.LookPath("pacman"); err != nil {
+			fmt.Println("Aurora currently supports Arch-based systems only. This machine does not appear to be Arch-based.")
+			os.Exit(1)
+		}
+		return nil
+	},
 }
 
 func Execute() {
